31ae205b89d6321e0d6b99f9af079e1898ba7dea
Odin H B <odinhb@protonmail.com>
2024-04-04 14:39:30 +0200

    Improve inspect output on Specdiff::Diff

--- lib/specdiff/diff.rb
+++ lib/specdiff/diff.rb
@@ -19,12 +19,15 @@
   end
 
   def inspect
-    if empty?
-      "<Specdiff::Diff (empty)>"
-    elsif raw.respond_to?(:bytesize)
-      "<Specdiff::Diff w/ #{raw&.bytesize || 0} bytes of #raw diff>"
-    else
-      "<Specdiff::Diff #{raw.inspect}>"
-    end
+    raw_diff = if empty?
+                 "empty"
+               elsif differ == ::Specdiff::Differ::Text
+                 bytes = raw&.bytesize || 0
+                 "#{bytes} bytes of #raw diff"
+               else
+                 "#{raw.inspect}"
+               end
+
+    "<Specdiff::Diff (#{a.type}/#{b.type}) (#{differ}) (#{raw_diff})>"
   end
 end
